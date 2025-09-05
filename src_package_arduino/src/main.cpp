f0f0d7b7e690919241db4cf26bffc4b8ffd4dd51
alexanderchasacademy <alexander.arvedson@chasacademy.se>
2025-09-05 12:55:25 +0200

    updated setup and tested the sensor readings

--- src_package_arduino/src/main.cpp
+++ src_package_arduino/src/main.cpp
@@ -4,10 +4,20 @@
 void setup()
 {
     Serial.begin(9600);
+    initializeDHT();
 }
 
 void loop()
 {
     Serial.println("Hello, Arduino!");
+
+    temperature = dht.readTemperature();
+    humidity = dht.readHumidity();
+
+    Serial.print("Temp: ");
+    Serial.print(temperature);
+    Serial.print(" °C, Humidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
     delay(1000);
 }
