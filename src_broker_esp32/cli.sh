6d80fbad333c2b88e57abf7cb117154c009a2ef6
Fredrik Storm <fredrik@fldc.se>
2025-09-09 23:11:39 +0200

    Simple script for CLI clangd use

---
+++ src_broker_esp32/cli.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# Simple script for switching clang between boards and different compile_commands on CLI
+
+PARAMETER=$1
+BOARD=$2
+
+if [ "$ENV" == "default" ]; then
+  BOARD="esp32_broker"
+elif [ "$BOARD" == "fredrik" ]; then
+  ENV="uno_r4_wifi"
+fi
+
+init() {
+  echo "Initializing PlatformIO CLI..."
+  if [ -z "$ENV" ]; then
+    pio init --ide vim && pio run -t compiledb -e uno_r4_wifi
+  else
+    pio init --ide vim && pio run -t compiledb -e "$ENV"
+  fi
+}
+
+compiledb() {
+  echo "Generating compile commands database..."
+  pio run -t compiledb
+}
+
+convert() {
+  echo -n "Converting compile_commands.json to alternate format.."
+  python3 ../tools/convert.py
+  echo "...done"
+}
+
+usage() {
+  echo "Usage: $0 [init|compiledb]"
+}
+
+if [ "$PARAMETER" == "init" ]; then
+  init
+elif [ "$PARAMETER" == "compiledb" ]; then
+  compiledb
+elif [ "$PARAMETER" == "convert" ]; then
+  convert
+else
+  usage
+  exit 1
+fi
