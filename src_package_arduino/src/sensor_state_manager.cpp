f3d60ef426c17bf94f1cf37d51560957f78b7517
alexanderchasacademy <alexander.arvedson@chasacademy.se>
2025-09-19 14:33:06 +0200

    implemented state_CreateAndBufferPacket logic

--- src_package_arduino/src/sensor_state_manager.cpp
+++ src_package_arduino/src/sensor_state_manager.cpp
@@ -7,8 +7,25 @@ sensor_state current_sensor_state = sensor_state::IDLE;
 // Manage and handle switching between different states
 void determineSensorState() {}
 
-// Implement later
-void state_CreateAndBufferPacket() {}
+// Create a new packets and adds it to the buffer
+void state_CreateAndBufferPacket()
+{
+    // Create new packet and add current data
+    SensorPacket newPacket = assembleSensorPacket();
+
+    // Try to add the packet to the buffer
+    if (!addPacketToBuffer(newPacket))
+    {
+        // Buffer full or other error
+        current_sensor_state = sensor_state::ERROR_STATE;
+    }
+
+    else
+    {
+        // Added succesfully to the buffer
+        current_sensor_state = sensor_state::IDLE;
+    }
+}
 
 // Implement later
 void state_TransferPacketBatch() {}
