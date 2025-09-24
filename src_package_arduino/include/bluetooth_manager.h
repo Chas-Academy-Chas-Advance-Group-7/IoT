02719c408a3aa7c7537b265389cd352f65a03951
alexanderchasacademy <alexander.arvedson@chasacademy.se>
2025-09-24 08:39:04 +0200

    Implemented 2 additional functions

--- src_package_arduino/include/bluetooth_manager.h
+++ src_package_arduino/include/bluetooth_manager.h
@@ -1,11 +1,17 @@
 #ifndef BLUETOOTH_MANAGER_H
 #define BLUETOOTH_MANAGER_H
 
+#include "sensor_package_manager.h"
 #include <Arduino.h>
 #include <ArduinoBLE.h>
 
 // UUIDs for service and characteristic
 constexpr char SENSOR_SERVICE_UUID[] = "b7cc8ff2-e60c-4d23-b54f-342371309812";
 constexpr char SENSOR_CHAR_UUID[] = "59dc9a5f-951f-47c4-a0a3-aac37796805f";
+
+// Function declarations
+bool setupBluetooth();
+bool isCentralConnected();
+BLECharacteristic &getSensorCharacteristic();
 
 #endif
--- src_package_arduino/src/bluetooth_manager.cpp
+++ src_package_arduino/src/bluetooth_manager.cpp
@@ -1,1 +1,41 @@
 #include "bluetooth_manager.h"
+
+// Define BLE service and characteristic at file scope
+BLEService sensorService(SENSOR_SERVICE_UUID);
+BLECharacteristic sensorChar(SENSOR_CHAR_UUID, BLERead | BLENotify, sizeof(SensorPacket));
+
+// Initialize BLE peripheral
+bool setupBluetooth()
+{
+    if (!BLE.begin())
+    {
+        Serial.println("Could not setup bluetooth.");
+        return false; // failed to setup bluetooth
+    }
+
+    // Set the local name to include the sensor ID
+    char deviceName[20];
+    sprintf(deviceName, "Sensor_%d", sensor_id);
+    BLE.setLocalName(deviceName);
+
+    // Setup service and characteristic
+    sensorService.addCharacteristic(sensorChar);
+    BLE.addService(sensorService);
+
+    BLE.setAdvertisedService(sensorService);
+    BLE.advertise();
+
+    return true;
+}
+
+// Check if a central device is connected
+bool isCentralConnected()
+{
+    return BLE.connected();
+}
+
+// Return a reference to the characteristic for sending packets elsewhere
+BLECharacteristic &getSensorCharacteristic()
+{
+    return sensorChar;
+}
