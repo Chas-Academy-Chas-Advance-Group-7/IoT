81c7ea70393c3f349743c302fb18843447a21d29
alexanderchasacademy <alexander.arvedson@chasacademy.se>
2025-09-24 07:53:11 +0200

    added bluetooth manager .h and .cpp

---
+++ src_package_arduino/include/bluetooth_manager.h
@@ -0,0 +1,11 @@
+#ifndef BLUETOOTH_MANAGER_H
+#define BLUETOOTH_MANAGER_H
+
+#include <Arduino.h>
+#include <ArduinoBLE.h>
+
+// UUIDs for service and characteristic
+constexpr char SENSOR_SERVICE_UUID[] = "b7cc8ff2-e60c-4d23-b54f-342371309812";
+constexpr char SENSOR_CHAR_UUID[] = "59dc9a5f-951f-47c4-a0a3-aac37796805f";
+
+#endif
