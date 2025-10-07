4e86b286fcf4eaf444a5ac14c2c1496ff6127a10
Fredrik Storm <fredrik@fldc.se>
2025-10-07 10:48:01 +0200

    chore(codebase): remove dead code and associated variables from networkstatus_task.cpp

--- src_broker_esp32/src/tasks/networkstatus_task.cpp
+++ src_broker_esp32/src/tasks/networkstatus_task.cpp
@@ -1,50 +1,20 @@
 #include "tasks/networkstatus_task.h"
-#include "sensor_data.h"
 #include "utils/threadsafe_serial.h"
 #include <Arduino.h>
 
 extern EventGroupHandle_t networkEventGroup;
 extern SemaphoreHandle_t networkEventMutex;
-extern QueueHandle_t networkQueue;
 
 #define NETWORK_CONNECTED_BIT BIT0
 
 void networkStatusTask(void *pvParameters)
 {
-    safePrintf("Network status task started - monitoring system health\n");
-
-    processed_data_t processedData;
-    uint32_t lastDataReceived = millis();
-    uint32_t dataCount = 0;
+    safePrintf("Network status task started - ready for network management\n");
 
     while (1)
     {
-        if (xQueueReceive(networkQueue, &processedData, pdMS_TO_TICKS(1000)) == pdTRUE)
-        {
-            lastDataReceived = millis();
-            dataCount++;
+        // TODO: Add WiFi
 
-            safePrintf("=== Network Queue Data ===\n");
-            safePrintf("Received JSON payload #%lu:\n", dataCount);
-            safePrintf("%s\n", processedData.json);
-            safePrintf("========================\n");
-        }
-        else
-        {
-            uint32_t timeSinceLastData = millis() - lastDataReceived;
-
-            if (timeSinceLastData > 30000)
-            {
-                safePrintf("WARNING: No sensor data received for %lu seconds\n",
-                           timeSinceLastData / 1000);
-            }
-
-            if (dataCount == 0)
-            {
-                safePrintf("System waiting for sensor data...\n");
-            }
-        }
-
-        vTaskDelay(pdMS_TO_TICKS(5000));
+        vTaskDelay(pdMS_TO_TICKS(10000));
     }
 }
