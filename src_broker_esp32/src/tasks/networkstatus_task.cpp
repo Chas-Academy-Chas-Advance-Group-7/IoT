8287e8130d07d9dfb5351c92e9a3c2b3c3db7592
alexanderchasacademy <alexander.arvedson@chasacademy.se>
2025-10-10 09:32:53 +0200

    added network connection functionality

--- src_broker_esp32/src/tasks/networkstatus_task.cpp
+++ src_broker_esp32/src/tasks/networkstatus_task.cpp
@@ -1,6 +1,8 @@
 #include "tasks/networkstatus_task.h"
+#include "WiFi_secrets.h"
 #include "utils/threadsafe_serial.h"
 #include <Arduino.h>
+#include <WiFi.h>
 
 extern EventGroupHandle_t networkEventGroup;
 extern SemaphoreHandle_t networkEventMutex;
@@ -9,11 +11,48 @@ extern SemaphoreHandle_t networkEventMutex;
 
 void networkStatusTask(void *pvParameters)
 {
-    safePrintf("Network status task started - ready for network management\n");
+    safePrintf("Network status task started.\n");
+
+    // Mode and credentials for Wifi connection
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    const int maxRetries = 20;
+    int retryCount = 0;
+
+    safePrintf("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED && retryCount < maxRetries)
+    {
+        vTaskDelay(pdMS_TO_TICKS(500));
+        safePrint(".");
+        retryCount++;
+    }
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        safePrintf("\nFailed to connect to WiFi.\n");
+    }
+    else
+    {
+        safePrintf("\nConnected! IP: %s\n", WiFi.localIP().toString().c_str());
+    }
+
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        // Signal system that network is ready
+        if (xSemaphoreTake(networkEventMutex, pdMS_TO_TICKS(1000)) == pdTRUE)
+        {
+            xEventGroupSetBits(networkEventGroup, NETWORK_CONNECTED_BIT);
+            xSemaphoreGive(networkEventMutex);
+        }
+    }
 
     while (1)
     {
-        // TODO: Add WiFi
+        if (WiFi.status() != WL_CONNECTED)
+        {
+            safePrintf("Wi-Fi lost! Reconnecting...\n");
+            WiFi.reconnect();
+        }
 
         vTaskDelay(pdMS_TO_TICKS(10000));
     }
