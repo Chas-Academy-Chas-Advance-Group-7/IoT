012c93905bb40c7330777827b157c1d8feea6918
alexanderchasacademy <alexander.arvedson@chasacademy.se>
2025-10-21 15:26:26 +0200

    refactored sensorpacket, added CA to central unit

--- src_broker_esp32/src/tasks/httptransmission_task.cpp
+++ src_broker_esp32/src/tasks/httptransmission_task.cpp
@@ -24,7 +24,7 @@
 #include "utils/threadsafe_serial.h"
 #include <HTTPClient.h>
 #include <WiFi.h>
-// #include <WiFiClientSecure.h> // Old secure client
+#include <WiFiClientSecure.h>
 
 /// Event group for network connection state.
 extern EventGroupHandle_t networkEventGroup;
@@ -46,6 +46,31 @@ extern QueueHandle_t networkQueue;
 
 /// Delay between retry attempts in milliseconds.
 #define RETRY_DELAY_MS 3000
+
+const char digicertG2[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIDjjCCAnagAwIBAgIQAzrx5qcRqaC7KGSxHQn65TANBgkqhkiG9w0BAQsFADBh
+MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
+d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH
+MjAeFw0xMzA4MDExMjAwMDBaFw0zODAxMTUxMjAwMDBaMGExCzAJBgNVBAYTAlVT
+MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j
+b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IEcyMIIBIjANBgkqhkiG
+9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuzfNNNx7a8myaJCtSnX/RrohCgiN9RlUyfuI
+2/Ou8jqJkTx65qsGGmvPrC3oXgkkRLpimn7Wo6h+4FR1IAWsULecYxpsMNzaHxmx
+1x7e/dfgy5SDN67sH0NO3Xss0r0upS/kqbitOtSZpLYl6ZtrAGCSYP9PIUkY92eQ
+q2EGnI/yuum06ZIya7XzV+hdG82MHauVBJVJ8zUtluNJbd134/tJS7SsVQepj5Wz
+tCO7TG1F8PapspUwtP1MVYwnSlcUfIKdzXOS0xZKBgyMUNGPHgm+F6HmIcr9g+UQ
+vIOlCsRnKPZzFBQ9RnbDhxSJITRNrw9FDKZJobq7nMWxM4MphQIDAQABo0IwQDAP
+BgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBhjAdBgNVHQ4EFgQUTiJUIBiV
+5uNu5g/6+rkS7QYXjzkwDQYJKoZIhvcNAQELBQADggEBAGBnKJRvDkhj6zHd6mcY
+1Yl9PMWLSn/pvtsrF9+wX3N3KjITOYFnQoQj8kVnNeyIv/iPsGEMNKSuIEyExtv4
+NeF22d+mQrvHRAiGfzZ0JFrabA0UWTW98kndth/Jsw1HKj2ZL7tcu7XUIOGZX1NG
+Fdtom/DzMNU+MeKNhJ7jitralj41E6Vf8PlwUHBHQRFXGU7Aj64GxJUTFy8bJZ91
+8rGOmaFvE7FBcf6IKshPECBV1/MUReXgRPTqh5Uykw7+U0b6LJ3/iyK5S9kJRaTe
+pLiaWN0bfVKfjllDiIGknibVb63dDcY3fe0Dkhvld1927jyNxF1WW6LZZm6zNTfl
+MrY=
+-----END CERTIFICATE-----
+)EOF";
 
 /**
  * @brief FreeRTOS task for sending sensor data over HTTP/HTTPS.
@@ -71,7 +96,7 @@ void httpTransmissionTask(void *pvParameters)
     // --- secure client setup ---
     static WiFiClientSecure client;
     static HTTPClient http;
-    client.setInsecure(); // disables certificate validation
+    client.setCACert(digicertG2);
 
     // HTTP client for testing (non-secure)
     // WiFiClient client;
